c9cdecd8bd67b7ef53e3c3c7cecf6dc55b486843
ekki6 <robbiejupiter@proton.me>
2026-07-15 20:48:08 +1000

    added conway.hs

---
+++ conway.hs
@@ -0,0 +1,41 @@
+--ugh, conway's game of life maybe eventually
+
+
+data Cell = Dead | Alive
+    deriving Show
+
+
+type Grid = [[Cell]]
+
+zeros :: Grid
+zeros =
+    [ [Dead ,Dead ,Dead ,Dead ,Dead ]
+    , [Dead ,Dead ,Dead ,Dead ,Dead ]
+    , [Dead ,Dead ,Dead ,Dead ,Dead ]
+    , [Dead ,Dead ,Dead ,Dead ,Dead ]
+    , [Dead ,Dead ,Dead ,Dead ,Dead ]
+    ]
+
+
+checkerboard :: Grid
+checkerboard =
+    [ [Dead ,Alive,Dead ,Alive,Dead ]
+    , [Alive,Dead ,Alive,Dead ,Alive]
+    , [Dead ,Alive,Dead ,Alive,Dead ]
+    , [Alive,Dead ,Alive,Dead ,Alive]
+    , [Dead ,Alive,Dead ,Alive,Dead ]
+    ]
+
+dispCell :: Cell -> Char
+dispCell Dead  = '.'
+dispCell Alive = '#'
+
+
+dispGrid :: Grid -> IO ()
+dispGrid g =
+    mapM_ putStrLn 
+        [ [ dispCell cell
+          | cell <- row
+          ]
+        | row <- g
+        ]
