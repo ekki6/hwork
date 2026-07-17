858b9ef7437654eeadc23708fc41577aca90e04e
ekki6 <robbiejupiter@proton.me>
2026-07-17 10:10:50 +1000

    edited primes

--- prime.hs
+++ prime.hs
@@ -6,7 +6,13 @@ lprimes = [2,3,5,7,11]
 tnums :: [Int]
 tnums = [1..20]
 
+isntDivBy :: Int -> Int -> Bool
+isntDivBy x y = rem x y /= 0
+
 isPrime :: Int -> Bool
 isPrime x = 
-    all (\p -> x `rem` p /= 0) lprimes
---    all  (`rem` x /= 0) lprimes
+--    all (\p -> rem x p /= 0) lprimes
+    all instDivBy lprimes
+
+pushLprimes :: a -> [a]
+pushLprimes 
