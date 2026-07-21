9bdb1f94e7e889bc6dad18cfa4299a1dcbd2ca27
ekki6 <robbiejupiter@proton.me>
2026-07-22 09:47:37 +1000

    yet another dioph edit

--- dioph_pe66.hs
+++ dioph_pe66.hs
@@ -4,5 +4,12 @@
 diTest :: Integer -> Integer -> Integer
 diTest d y = d * y * y + 1
 
+-- takes the sqrt() of a n and floors it
+-- producing a new number which, if n is a perfect square,
+-- when squared should give us back n
 testSqr :: Integer -> Integer
 testSqr x = floor (sqrt (fromIntegral x :: Double)) 
+
+isSqr :: Integer -> Bool
+isSqr x = (testSqr x)^2 == x
+
