f4fac36190f3da76d6aa869c20586fb88238d958
ekki6 <robbiejupiter@proton.me>
2026-07-21 18:54:23 +1000

    added dioph_pe66.hs

---
+++ dioph_pe66.hs
@@ -0,0 +1,8 @@
+--for project euler #66 on Diophantine equations
+--find minimal solution for Diophantine equation x^2 - Dy^2 = 1
+
+diTest :: Integer -> Integer -> Integer
+diTest d y = d * y * y + 1
+
+--testSqr :: Integer -> Double
+--testSqr x = floor (sqrt (x :: Double)) 
