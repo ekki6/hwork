fd167f10a89e6d8f014cc771995e94c3fc144a98
ekki6 <robbiejupiter@proton.me>
2026-07-22 14:28:49 +1000

    yet another dioph edit

--- dioph_pe66.hs
+++ dioph_pe66.hs
@@ -1,6 +1,8 @@
 --for project euler #66 on Diophantine equations
 --find minimal solution for Diophantine equation x^2 - Dy^2 = 1
 
+import Data.List (find)
+
 diTest :: Integer -> Integer -> Integer
 diTest d y = d * y * y + 1
 
@@ -12,4 +14,11 @@ testSqr x = floor (sqrt (fromIntegral x :: Double))
 
 isSqr :: Integer -> Bool
 isSqr x = (testSqr x)^2 == x
+
+--so interactively do
+--ghci> find (\y -> isSqr (diTest 41 y)) [1..2000]
+--Just 320
+
+getY :: Integer -> Maybe Integer
+getY dee = find (\y -> isSqr (diTest dee y)) [1..2000]
 
