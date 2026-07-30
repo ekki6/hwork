c861da1136a3badddd8278ec506710fd94aa3f3f
robbie <robbiejupiter@proton.me>
2026-07-29 22:04:53 -0700

    started kubgen.hs

---
+++ kubgen.hs
@@ -0,0 +1,34 @@
+-- kubgen in haskell
+
+hsh :: String
+hsh= concat
+    [ "7ef0742612e4db668ea2a7b5779f69cf"
+    , "13762b48d36812fd72eda69207460a71"
+    , "d2e2c901b8f46991249bd566e98b9e2b"
+    , "31e897029c54c20f57a31e8e23f452f"
+    ]
+
+hexToFour :: Char -> [Char]
+hexToFour '0' = "00"
+hexToFour '1' = "01"
+hexToFour '2' = "02"
+hexToFour '3' = "03"
+hexToFour '4' = "10"
+hexToFour '5' = "11"
+hexToFour '6' = "12"
+hexToFour '7' = "13"
+hexToFour '8' = "20"
+hexToFour '9' = "21"
+hexToFour 'a' = "22"
+hexToFour 'b' = "23"
+hexToFour 'c' = "30"
+hexToFour 'd' = "31"
+hexToFour 'e' = "32"
+hexToFour 'f' = "33"
+hexToFour c = error ("Not a hex digit" ++ [c])
+
+fourHash :: String -> String
+fourHash = concatMap hexToFour
+
+
+
